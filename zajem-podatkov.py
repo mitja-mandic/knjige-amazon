ae2303ea0fc3b3e89f62ad284fb85b15568c32c9
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-10-20 22:01:33 +0200

    delo na zajemu. Uporabu pristop iz vaj, DELUJE

--- zajem-podatkov.py
+++ zajem-podatkov.py
@@ -8,12 +8,6 @@ orodja.shrani_spletno_stran(link,"podatki/naslovna.html") #naslovno stran rabimo
 vsebina = orodja.vsebina_datoteke("podatki/naslovna.html")
 juha = bs4.BeautifulSoup(vsebina, 'html.parser')
 
-#vzorec_linka = re.compile()
-
-#for i in juha.find_all('a', 'href'):
-#    print(i)^
-
-
 
 
 stevilo_strani = 5
@@ -27,18 +21,61 @@ for stran in range(1,stevilo_strani):
         url = f"https://www.nepremicnine.net/oglasi-prodaja/{stran}/"
         datoteka = f"podatki/nepremicnine{stran}.html"
         orodja.shrani_spletno_stran(url, datoteka)
-#print(count)
+
+filename = r'podatki\nepremicnine1.html'
+def oglasi(datoteka):
+    vzorec_oglasa = re.compile(
+    r'oglasbold(.*?)<meta itemprop="priceCurrency" content="EUR" />', 
+    re.DOTALL)
+    vsebina_datoteke = orodja.vsebina_datoteke(datoteka)
+    return re.findall(vzorec_oglasa, vsebina_datoteke)
 
 
-page = orodja.vsebina_datoteke("nepremicnine1.html")
-soup = bs4.BeautifulSoup(page, 'html.parser')
-with open("podatki/test.html",'w', encoding='utf-8') as d:
-    d.write(soup.prettify())
+def slovar_iz_oglasa(oglas):
+    vzorec = re.compile(
+        r'a href=".*?title="(?P<id>(\d{7}))' 
+        r'.+?'
+        r'span class="title">(?P<ime_oglasa>(.*?))<'
+        r'.+?'
+        r'<span class="vrsta">(?P<vrsta_nepremicnine>(.*?))<',
+    re.DOTALL)
+    rezultat = re.search(vzorec, oglas)
+    return rezultat.groupdict()
 
-for povezava in soup.find_all('h2'):
-    link = povezava.get('data-href')
-    id = int(re.search(r'\d{7}', link).group(0))
-    lokacija = list(povezava.descendants)[-1]
-    #print(id, lokacija)
+def oglasi_iz_datoteke(datoteka):
+    return[slovar_iz_oglasa(oglas) for oglas in oglasi(datoteka)]
 
-seznam_lokacij = [lokacija.get_text() for lokacija in soup.find_all('span', class_="title")]
+#test_vzorec = re.compile(
+#        r'a href=".*?title="(?P<id>(\d{7}))' 
+#        r'.+?'
+#        r'span class="title">(?P<ime_oglasa>(.*?))<'
+#        r'.+?'
+#        r'<span class="vrsta">(?P<vrsta_nepremicnine>(.*?))<',
+#    re.DOTALL)
+#
+#print(re.search(test_vzorec,"primer_oglasa.html").groupdict())
+
+
+
+
+
+
+
+
+
+
+
+#print(re.search(vzorec,orodja.vsebina_datoteke(r"podatki\nepremicnine1.html")).groupdict())
+
+#page = orodja.vsebina_datoteke("nepremicnine1.html")
+#soup = bs4.BeautifulSoup(page, 'html.parser')
+#with open("podatki/test.html",'w', encoding='utf-8') as d:
+#    d.write(soup.prettify())
+#
+#for povezava in soup.find_all('h2'):
+#    link = povezava.get('data-href')
+#    id = int(re.search(r'\d{7}', link).group(0))
+#    lokacija = list(povezava.descendants)[-1]
+#    #print(id, lokacija)
+#
+#seznam_lokacij = [lokacija.get_text() for lokacija in soup.find_all('span', class_="title")]
