1f3a8a37465b07427cfed0147a2d912d75f4b2ac
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-10-22 11:45:56 +0200

    Zajem strani s podatki narejen

--- zajem-podatkov.py
+++ zajem-podatkov.py
@@ -3,31 +3,45 @@ import re
 import requests
 import bs4
 
-link = "https://www.nepremicnine.net/"
-orodja.shrani_spletno_stran(link,"podatki/naslovna.html") #naslovno stran rabimo za regije
-vsebina = orodja.vsebina_datoteke("podatki/naslovna.html")
-#juha = bs4.BeautifulSoup(vsebina, 'html.parser')
 
-stevilo_strani = 5
-for stran in range(1,stevilo_strani):
-    if stran == 1:
-        datoteka = f"podatki/nepremicnine{stran}.html"
-        url = f"https://www.nepremicnine.net/oglasi-prodaja/"
-        orodja.shrani_spletno_stran(url, datoteka)
-        vsebina = orodja.vsebina_datoteke(datoteka)
-    else:
-        url = f"https://www.nepremicnine.net/oglasi-prodaja/{stran}/"
-        datoteka = f"podatki/nepremicnine{stran}.html"
-        orodja.shrani_spletno_stran(url, datoteka)
+def zajemi_strani_z_oglasi(st_strani, url):
+    seznam_linkov = []
+    vzorec_linka = re.compile(r'<!--<meta itemprop="url" content="(.*?)"',re.DOTALL)
+    for stran in range(1,st_strani):
+        if stran == 1:
+            datoteka = f"podatki/nepremicnine{stran}.html"
+            #url = f"https://www.nepremicnine.net/oglasi-prodaja/"
+            orodja.shrani_spletno_stran(url, datoteka)
+            seznam_linkov += re.findall(vzorec_linka, orodja.vsebina_datoteke(datoteka))
+            print(seznam_linkov)
+        else:
+            novi_url = url + f"{stran}/"
+            datoteka = f"podatki/nepremicnine{stran}.html"
+            orodja.shrani_spletno_stran(novi_url, datoteka)
+            seznam_linkov += re.findall(vzorec_linka, orodja.vsebina_datoteke(datoteka))
+    return seznam_linkov
+
+def zajemi_oglase(seznam_linkov):
+    st = 1
+    for link in seznam_linkov:
+        datoteka = f'oglas{st}.html'
+        orodja.shrani_spletno_stran(link, datoteka)
+        st += 1
+
+
+stevilo_strani = 550
+url = r"https://www.nepremicnine.net/oglasi-prodaja/slovenija/"
+seznam_linkov = zajemi_strani_z_oglasi(stevilo_strani, url)
+
 
 filename = r'podatki\nepremicnine1.html'
+#tega spodi verjetno ne rabiš
 def oglasi(datoteka):
     vzorec_oglasa = re.compile(
     r'oglasbold(.*?)<meta itemprop="priceCurrency" content="EUR" />', 
     re.DOTALL)
     vsebina_datoteke = orodja.vsebina_datoteke(datoteka)
     return re.findall(vzorec_oglasa, vsebina_datoteke)
-
 
 def slovar_iz_oglasa(oglas):
     vzorec = re.compile(
@@ -42,25 +56,6 @@ def slovar_iz_oglasa(oglas):
 
 def oglasi_iz_datoteke(datoteka):
     return[slovar_iz_oglasa(oglas) for oglas in oglasi(datoteka)]
-
-
-def zajemi_podrobne_oglase(datoteka):
-    seznam_id = oglasi_iz_datoteke(datoteka)
-    for slovar in seznam_id:
-        stevilka = int(slovar['id'])
-        
-        vsta = slovar['vrsta_nepremicnine'].split()
-        niz_vrste = ''
-        for beseda in vrsta:
-            if beseda[-1] == ',':
-                beseda = beseda[:-1]
-            niz_vrste += beseda + '-'
-        
-        ime = slovar[vrsta_nepremicnine].split().strip()
-        url = f'https://www.nepremicnine.net/oglasi-prodaja/{niz_vrste}-{2+2}_{stevilka}/'
-
-
-
 
 
 
