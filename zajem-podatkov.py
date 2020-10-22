600ebf5deb7ca506cfb9cdb25e8ddbb521e71861
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-10-22 22:55:23 +0200

    Nova mapa za podatke. Regije se da iz že pobranih dobit, zgleda bo treba še enkrat

--- zajem-podatkov.py
+++ zajem-podatkov.py
@@ -5,43 +5,50 @@ import bs4
 
 
 def zajemi_strani_z_oglasi(st_strani, url):
-    seznam_linkov = []
-    vzorec_linka = re.compile(r'<!--<meta itemprop="url" content="(.*?)"',re.DOTALL)
+    
     for stran in range(1,st_strani):
         if stran == 1:
-            datoteka = f"podatki/nepremicnine{stran}.html"
+            datoteka = f"strani_z_oglasi\\podatki\\nepremicnine{stran}.html"
             #url = f"https://www.nepremicnine.net/oglasi-prodaja/"
             orodja.shrani_spletno_stran(url, datoteka)
-            seznam_linkov += re.findall(vzorec_linka, orodja.vsebina_datoteke(datoteka))
-            print(seznam_linkov)
         else:
             novi_url = url + f"{stran}/"
-            datoteka = f"podatki/nepremicnine{stran}.html"
+            datoteka = f"strani_z_oglasi\\podatki\\nepremicnine{stran}.html"
             orodja.shrani_spletno_stran(novi_url, datoteka)
-            seznam_linkov += re.findall(vzorec_linka, orodja.vsebina_datoteke(datoteka))
+            
+def poberi_regije(datoteka):
+    vzorec_regije = re.compile(
+        r'Vse regije</a>.*?>'
+        r'(.*?)'
+        r'<\a>'
+        ,#r'(.*)<\a>',
+    re.DOTALL)
+    vsebina = orodja.vsebina_datoteke(datoteka)
+    return re.findall(vzorec_regije, vsebina)
+
+def poberi_linke(st_datotek):
+    seznam_linkov = []
+    for fajl in range(1,st_datotek):
+        dat = f'podatki\\ strani_z_oglasi\\nepremicnine{fajl}.html'
+        vsebina = orodja.vsebina_datoteke(dat)
+        vzorec_linka = re.compile(r'<!--<meta itemprop="url" content="(.*?)"',re.DOTALL)
+        seznam_linkov += re.findall(vzorec_linka, vsebina)
     return seznam_linkov
 
 def zajemi_oglase(seznam_linkov):
     st = 1
-    for link in seznam_linkov:
-        datoteka = f'oglas{st}.html'
-        orodja.shrani_spletno_stran(link, datoteka)
+    for url in seznam_linkov:
+        datoteka = f'podatki\\oglasi\\oglas{st}.html'
+        orodja.shrani_spletno_stran(url, datoteka)
         st += 1
 
 
 stevilo_strani = 550
 url = r"https://www.nepremicnine.net/oglasi-prodaja/slovenija/"
-seznam_linkov = zajemi_strani_z_oglasi(stevilo_strani, url)
+#seznam_linkov = poberi_linke(stevilo_strani)
+filename = r'podatki\strani_z_oglasi\nepremicnine1.html'
 
-
-filename = r'podatki\nepremicnine1.html'
-#tega spodi verjetno ne rabiš
-def oglasi(datoteka):
-    vzorec_oglasa = re.compile(
-    r'oglasbold(.*?)<meta itemprop="priceCurrency" content="EUR" />', 
-    re.DOTALL)
-    vsebina_datoteke = orodja.vsebina_datoteke(datoteka)
-    return re.findall(vzorec_oglasa, vsebina_datoteke)
+print(poberi_regije(filename))
 
 def slovar_iz_oglasa(oglas):
     vzorec = re.compile(
@@ -54,48 +61,19 @@ def slovar_iz_oglasa(oglas):
     rezultat = re.search(vzorec, oglas)
     return rezultat.groupdict()
 
-def oglasi_iz_datoteke(datoteka):
-    return[slovar_iz_oglasa(oglas) for oglas in oglasi(datoteka)]
 
 
 
 
 
 
-
-
-
-#test_vzorec = re.compile(
-#        r'a href=".*?title="(?P<id>(\d{7}))' 
-#        r'.+?'
-#        r'span class="title">(?P<ime_oglasa>(.*?))<'
-#        r'.+?'
-#        r'<span class="vrsta">(?P<vrsta_nepremicnine>(.*?))<',
+#def oglasi(datoteka):
+#    vzorec_oglasa = re.compile(
+#    r'oglasbold(.*?)<meta itemprop="priceCurrency" content="EUR" />', 
 #    re.DOTALL)
+#    vsebina_datoteke = orodja.vsebina_datoteke(datoteka)
+#    return re.findall(vzorec_oglasa, vsebina_datoteke)
 #
-#print(re.search(test_vzorec,"primer_oglasa.html").groupdict())
-
-
-
-
-
-
-
-
-
-
-
-#print(re.search(vzorec,orodja.vsebina_datoteke(r"podatki\nepremicnine1.html")).groupdict())
-
-#page = orodja.vsebina_datoteke("nepremicnine1.html")
-#soup = bs4.BeautifulSoup(page, 'html.parser')
-#with open("podatki/test.html",'w', encoding='utf-8') as d:
-#    d.write(soup.prettify())
 #
-#for povezava in soup.find_all('h2'):
-#    link = povezava.get('data-href')
-#    id = int(re.search(r'\d{7}', link).group(0))
-#    lokacija = list(povezava.descendants)[-1]
-#    #print(id, lokacija)
-#
-#seznam_lokacij = [lokacija.get_text() for lokacija in soup.find_all('span', class_="title")]
+#def oglasi_iz_datoteke(datoteka):
+#    return[slovar_iz_oglasa(oglas) for oglas in oglasi(datoteka)]
