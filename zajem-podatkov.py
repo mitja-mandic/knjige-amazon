b0270e33cd3e8dee5a33cf44ad190513888b0d39
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-10-24 20:26:29 +0200

    Zajem podatkov končan pomoje, mal še dela z obdelavo

--- zajem-podatkov.py
+++ zajem-podatkov.py
@@ -2,6 +2,7 @@ import orodja
 import re
 import requests
 import bs4
+import os
 
 def zajemi_strani_z_oglasi(st_strani, url):
     for stran in range(1,st_strani):
@@ -54,10 +55,10 @@ def st_oglasov_na_regijo(url): #koliko strani z oglasi je v vsaki regiji
     stevilo = re.findall(vzorec_st, vsebina)
     return (ime, int(stevilo[0]))
 
-def zajemi_po_regiji(url): #shrani prvo stran vsake regije
-    regija = st_oglasov_na_regijo(url)[0]
-    stevilo_strani = st_oglasov_na_regijo(url)[1]
-    for stran in range(2, stevilo_strani + 1):
+def zajemi_po_regiji(url, stevilo_oglasov, regija): #shrani po regijah
+#    regija = st_oglasov_na_regijo(url)[0]
+#    stevilo_strani = st_oglasov_na_regijo(url)[1]
+    for stran in range(2, stevilo_oglasov + 1):
         datoteka = f'podatki\\oglasi\\oglas_{regija}_{stran}.html'
         nov_url = url + f'{stran}/'
         orodja.shrani_spletno_stran(nov_url, datoteka)
@@ -70,27 +71,35 @@ def upravne_enote(datoteka_regije): #pobere linke za upravne enote iz prve stran
     )
     return re.findall(vzorec_linka, vsebina)
 
+url_naslovna = r"https://www.nepremicnine.net/oglasi-prodaja/slovenija/"
+datoteka_naslovna = r'podatki\strani_z_oglasi\nepremicnine1.html'
 
-stevilo_strani = 550
-url = r"https://www.nepremicnine.net/oglasi-prodaja/slovenija/"
-
-filename = r'podatki\strani_z_oglasi\nepremicnine1.html'
-regije = poberi_regije(filename)
+seznam = poberi_regije(datoteka_naslovna)
 
 def zajemi_regije(seznam):
     for link in seznam:
-        zajemi_po_regiji(link)
+        zajemi_po_regiji(link, st_oglasov_na_regijo(link)[1], st_oglasov_na_regijo(link)[0])
 
-zajemi_regije(regije)
-
+#zajemi_regije(seznam) #Datoteke so pobrane
 
 def slovar_iz_oglasa(oglas): #naredi slovar iz seznama oglasov.
     vzorec = re.compile(
-        r'a href=".*?title="(?P<id>(\d{7}))' 
+        r'<meta itemprop="category" content="oglasi prodaja > (?P<regija>(.*?))\s'
+        r'.*?'
+        r'a href=".*?title="(?P<id>(\d{7}))'
         r'.+?'
         r'span class="title">(?P<ime_oglasa>(.*?))<'
         r'.+?'
-        r'<span class="vrsta">(?P<vrsta_nepremicnine>(.*?))<',
+        r'<span class="vrsta">(?P<vrsta_nepremicnine>(.*?))'
+        r'</span><span class="tipi">(?P<tip_nepremicnine>(.*?))<'
+        r'.+?'
+        r'<span class="atribut">Zemljišče: <strong>(?P<zemljisce>(.+?))\s'
+        r'.+?'
+        r'<span class="velikost" lang="sl">(?P<velikost>(.*?))\sm'
+        r'.+?'
+        r'<span class="cena">(?P<cena>(.+?))\s&euro;</span>'
+        r'\s+'
+        r'<span class="agencija">(?P<agencija>(.*?))</span>',
     re.DOTALL)
     rezultat = re.search(vzorec, oglas)
     return rezultat.groupdict()
@@ -99,15 +108,12 @@ def slovar_iz_oglasa(oglas): #naredi slovar iz seznama oglasov.
 
 
 
+def oglasi(datoteka):
+    vzorec_oglasa = re.compile(
+    r'oglasbold(.*?)<meta itemprop="priceCurrency" content="EUR" />', 
+    re.DOTALL)
+    vsebina_datoteke = orodja.vsebina_datoteke(datoteka)
+    return re.findall(vzorec_oglasa, vsebina_datoteke)
 
-
-#def oglasi(datoteka):
-#    vzorec_oglasa = re.compile(
-#    r'oglasbold(.*?)<meta itemprop="priceCurrency" content="EUR" />', 
-#    re.DOTALL)
-#    vsebina_datoteke = orodja.vsebina_datoteke(datoteka)
-#    return re.findall(vzorec_oglasa, vsebina_datoteke)
-#
-#
-#def oglasi_iz_datoteke(datoteka):
-#    return[slovar_iz_oglasa(oglas) for oglas in oglasi(datoteka)]
+def oglasi_iz_datoteke(datoteka):
+    return[slovar_iz_oglasa(oglas) for oglas in oglasi(datoteka)]
